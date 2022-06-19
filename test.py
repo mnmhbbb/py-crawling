0787ea5dccdcb7dc809bdda2ac2b1e4df0ef8427
mnmhbbb <mnmhb64@gmail.com>
2022-06-19 19:06:07 +0900

    기존 엑셀 파일에 데이터 추가하기

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import openpyxl
 import requests
 from bs4 import BeautifulSoup
+import openpyxl
+
+fpath = r'/Users/mhbaek/dev/python/test-1.xlsx'
+wb = openpyxl.load_workbook(fpath)
+ws = wb.active # 현재 활성화된 시트
 
 codes = [
   '005930',
@@ -7,6 +13,7 @@ codes = [
   '035720'
 ]
 
+row = 2
 for code in codes:
   url = f'https://finance.naver.com/item/sise.naver?code={code}'
   res = requests.get(url)
@@ -14,4 +21,7 @@ for code in codes:
   soup = BeautifulSoup(html, 'html.parser')
   price = soup.select_one('#_nowVal').text
   print(price)
+  ws[f'B{row}'] = price
+  row = row + 1
 
+wb.save(fpath)
